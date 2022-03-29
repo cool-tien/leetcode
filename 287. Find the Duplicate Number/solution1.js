7208d284c43563daf952063fd2ac244ed2f7a900
cool0o0 <coolmantien@gmail.com>
2022-03-29 10:27:54 +0800

    Update (2022-03-29)

---
+++ 287. Find the Duplicate Number/solution1.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findDuplicate = function(nums) {
+    let set = new Set();
+    
+    for(let num of nums)
+        if(set.has(num))
+            return num;
+        else
+            set.add(num);
+};
