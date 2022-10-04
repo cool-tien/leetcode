711cb4e33fb1c48b1965b100046a3e5214a68afc
cool <coolmantien@gmail.com>
2022-10-04 10:14:31 +0800

    Train JS

--- 1668. Maximum Repeating Substring/solution1.js
+++ 1668. Maximum Repeating Substring/solution1.js
@@ -1,4 +1,18 @@
 /**
+    O(n²)
+    97~115 ms    (12.93%~47.41%)
+    42.2~43.1 MB (6.90%~51.72%)
+
+    https://leetcode.com/submissions/detail/814036137/
+        Runtime: 115 ms, faster than 12.93% of JavaScript online submissions for Maximum Repeating Substring.
+        Memory Usage: 43.1 MB, less than 6.90% of JavaScript online submissions for Maximum Repeating Substring.
+    https://leetcode.com/submissions/detail/814640885/
+        Runtime: 106 ms, faster than 32.76% of JavaScript online submissions for Maximum Repeating Substring.
+        Memory Usage: 42.6 MB, less than 6.90% of JavaScript online submissions for Maximum Repeating Substring.
+    https://leetcode.com/submissions/detail/814641698/
+        Runtime: 97 ms, faster than 47.41% of JavaScript online submissions for Maximum Repeating Substring.
+        Memory Usage: 42.2 MB, less than 51.72% of JavaScript online submissions for Maximum Repeating Substring.
+
  * @param {string} sequence
  * @param {string} word
  * @return {number}
