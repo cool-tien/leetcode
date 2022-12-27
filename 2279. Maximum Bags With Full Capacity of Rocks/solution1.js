61b899f604134186d7a1eedd02ddff18ca5f9d44
cool <coolmantien@gmail.com>
2022-12-27 14:30:46 +0800

    Includes Time and Space Complexity

--- 2279. Maximum Bags With Full Capacity of Rocks/solution1.js
+++ 2279. Maximum Bags With Full Capacity of Rocks/solution1.js
@@ -1,4 +1,17 @@
 /**
+    Time Complexity: O(n log n)
+        150 ~ 188 ms    (80.00% ~ 96.00%)
+    Space Complexity: O(n)
+        56.9 ~ 57.4 MB  (44.00%)
+
+    https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks/submissions/866177296/
+        Runtime 150 ms, Beats 96.00%; Memory 57.4 MB, Beats 44.00%
+    https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks/submissions/866179294/
+        Runtime 164 ms, Beats 92.00%; Memory 57.2 MB, Beats 44.00%
+    https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks/submissions/866179717/
+        Runtime 188 ms, Beats 80.00%; Memory 56.9 MB, Beats 44.00%
+
+ * 
  * @param {number[]} capacity
  * @param {number[]} rocks
  * @param {number} additionalRocks
@@ -6,7 +19,7 @@
  */
 var maximumBags = function(capacity, rocks, additionalRocks) {
     let diffs = [];
-    let output = 0;
+    let full = 0;
 
     for(let i=0; i<capacity.length; i++)
         diffs.push(capacity[i] - rocks[i]);
@@ -16,10 +29,10 @@ var maximumBags = function(capacity, rocks, additionalRocks) {
     for(let diff of diffs)
         if(additionalRocks >= diff){
             additionalRocks -= diff;
-            output++;
+            full++;
         }
         else
             break;
 
-    return output;
+    return full;
 };
